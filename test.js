f3140f297cf524eeaed9997ee34c1f2bd87e809d
faziii <fazi5486@gmail.com>
2023-11-14 09:22:26 +0500

    added

---
+++ test.js
@@ -0,0 +1,28 @@
+
+{
+let V1 = 34567;
+console.log(V1);
+let V2=76543;
+console.log(V2);
+{
+    
+     let V1=34567;
+      console .log(V1);
+
+    }
+   console.log(V1);
+}
+
+
+console.log("IF ELSE")
+{
+    let V1=1
+    let V2=2
+    if(V1=V2){
+        console.log("V2=1234");
+    
+    }
+    else{
+       console.log("V2=0")
+    }
+}
